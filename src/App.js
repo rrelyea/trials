f0575c2159d0b22ef6e558b2acd347522d079da7
Rob Relyea <rob@relyeas.net>
2022-04-30 10:17:02 -0700

    sort in reverse order. add '-' after phase in key. replace startDate with updatedDate in key.

--- src/App.js
+++ src/App.js
@@ -28,21 +28,15 @@ class Trials extends React.Component {
     var trials = {};
     data.StudyFieldsResponse.StudyFields.forEach((trial) => {
       if (trial.StartDate !== "" && trial.CompletionDate !== "" ) {
-        var StartDate = null;
-        if (trial.StartDate[0] !== "") {
-          StartDate = new Date(trial.StartDate[0]);
-        } else if (trial.StudyFirstPostDate !== "") {
-          StartDate = new Date(trial.StudyFirstPostDate[0]);
-        }
+        var LastUpdatePostDate = LastUpdatePostDate !== null ? new Date(trial.LastUpdatePostDate) : null;
 
         var CompletionDate = new Date(trial.CompletionDate);
-        if (this.startViewDate == null || this.startViewDate > StartDate) this.startViewDate = StartDate;
-        if (this.endViewDate == null || this.endViewDate < CompletionDate) this.endViewDate = CompletionDate;
         var phaseStr = "";
         var phase = "9";
         if (trial.phase !== null) {
           phaseStr = (trial.Phase[0] !== null && trial.Phase[0] !== undefined) ? trial.Phase[0].toString() : "";
           phase = phaseStr.substring(phaseStr.length - 1);
+          if (phase == "e") phase = "0";
         }
         var completed = trial.OverallStatus == "Completed";
         var unknown = trial.OverallStatus == "Unknown status";
@@ -53,8 +47,9 @@ class Trials extends React.Component {
         else if (unknown) { status = "unknown"}
         else if (terminated) {status = "terminated"}
         trial.status = status;
-        var datestring = StartDate !== null ? StartDate.getFullYear() + StartDate.getMonth() + StartDate.getDay() : "        ";
-        var key = phase + datestring + trial.LeadSponsorName;
+        var datestring = LastUpdatePostDate !== null ? (LastUpdatePostDate.getFullYear() + ("0" + (LastUpdatePostDate.getMonth() + 1)).slice(-2)) : "        ";
+        var key = phase +"-"+ datestring + trial.LeadSponsorName;
+        trial.key = key;
         trials[key] = trial;
 
       }
@@ -63,8 +58,8 @@ class Trials extends React.Component {
     function sortKeys(obj_1) {
       var key = Object.keys(obj_1)
       .sort(function order(key1, key2) {
-          if (key1 < key2) return -1;
-          else if (key1 > key2) return +1;
+          if (key1 < key2) return +1;
+          else if (key1 > key2) return -1;
           else return 0;
       }); 
         
@@ -106,8 +101,6 @@ class Trials extends React.Component {
                 <div><span>{trial.LeadSponsorName}</span>: <span>{trial.InterventionName[0]}</span></div>
                 <div><a href={'https://beta.clinicaltrials.gov/study/'+trial.NCTId[0]}>{trial.NCTId[0]}</a> : <span>{trial.BriefTitle}</span></div>
                 <span>{phase}</span>  <span>{trial.OverallStatus}</span>  <span>{trial.WhyStopped}</span><br/>
-                <span>Start: {trial.StartDate}</span> - <span>FirstPost: {trial.StudyFirstPostDate}</span> - <span>LastUpdate: {trial.LastUpdatePostDate}</span> - <span>CompletionDate: {trial.CompletionDate}</span>
-                <div>&nbsp;</div>
               </div></>
           })
         : false}  
