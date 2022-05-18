521f4ba6597bb6f7f5a0e255c3a03e987d7ca295
Rob Relyea <rob@relyeas.net>
2022-05-17 19:32:17 -0700

    added several sorts and views, some for demo.

--- src/TrialUtilities.js
+++ src/TrialUtilities.js
@@ -8,17 +8,24 @@
         return await GetData(pubmedUrl);
     }
 
-    export async function fetchTrialsData(query) {     
+    export async function fetchTrialsData(query, dataAnnotations) {     
       var moreToGet = 1;
       var maxRank = 999;
       var minRank = 1;
       var trials = [];
       var trialCount = 0;
   
+      var annotationsByTrial = {};
+      if (dataAnnotations != null) {
+        for (var i = 0; i < dataAnnotations.data.length; i++) {
+            annotationsByTrial[dataAnnotations.data[i].trial] = dataAnnotations.data[i];
+        }
+      }
+
       while (moreToGet > 0) {
-        var url = 'https://clinicaltrials.gov/api/query/study_fields?expr='+query+'&fields=NCTId,Condition,BriefTitle,StudyType,Phase,OverallStatus,WhyStopped,LeadSponsorName,InterventionName,StudyFirstPostDate,StartDate,StartDateType,LastUpdatePostDate,PrimaryCompletionDate,CompletionDate&fmt=JSON&min_rnk='+minRank.toString()+'&max_rnk='+maxRank.toString();
+        var url = 'https://clinicaltrials.gov/api/query/study_fields?expr='+query+'&fields=NCTId,Condition,LocationFacility,BriefTitle,StudyType,Phase,OverallStatus,WhyStopped,LeadSponsorName,InterventionName,StudyFirstPostDate,StartDate,StartDateType,LastUpdatePostDate,PrimaryCompletionDate,CompletionDate&fmt=JSON&min_rnk='+minRank.toString()+'&max_rnk='+maxRank.toString();
         var data = await GetData(url);
-        if ('StudyFields' in data.StudyFieldsResponse) {
+        if (data.StudyFieldsResponse != null && 'StudyFields' in data.StudyFieldsResponse) {
             trialCount = data.StudyFieldsResponse.NStudiesFound;
             data.StudyFieldsResponse.StudyFields.forEach((trial) => {
             var CompletionDate = null
@@ -31,7 +38,7 @@
             }
             trial.endDate = CompletionDate;
             trial.phaseInfo = getPhaseInfo(trial.Phase[trial.Phase.length-1], trial.Phase[0], trial.StudyType);
-
+            trial.closed = getClosedInfo(trial);
             switch (trial.OverallStatus.toString()) 
             {
                 case "Enrolling by invitation":
@@ -47,7 +54,11 @@
                     trial.OverallStatusStyle = trial.OverallStatus;
                     break;
             }
-  
+
+            if (trial.NCTId in annotationsByTrial) {
+                trial.annotations = annotationsByTrial[trial.NCTId];
+            } 
+
             trials.push(trial);
           });
         }
@@ -67,48 +78,57 @@
       return trials;
     }
 
+    export function getClosedInfo(trial) {
+        return  !(trial.OverallStatus.toString() != "Completed" &&
+        trial.OverallStatus.toString() != "No longer available" && 
+        trial.OverallStatus.toString() != "Unknown status" && 
+        trial.OverallStatus.toString() != "Withdrawn" && 
+        trial.OverallStatus.toString() != "Terminated");
+    }
+
     export function getPhaseInfo(lastPhase, firstPhase, studyType) {
-        var phaseInfo = {group: lastPhase};
+        var phaseInfo = {name: lastPhase};
 
         if (lastPhase !== null) {
             switch (lastPhase) {
                 case 'Phase 1':
-                    phaseInfo.order = "1X";
+                    phaseInfo.number = 1.5;
                     break;
                 case 'Early Phase 1':
-                    phaseInfo.order = "1A";
+                    phaseInfo.number = 1.0;
                     break;
                 case 'Phase 2':
                     if (firstPhase == 'Phase 1') {
-                        phaseInfo.order = "2A";
-                        phaseInfo.group = "Phase 1/2";
+                        phaseInfo.name = "Phase 1/2";
+                        phaseInfo.number = 2.25;
                     } else {
-                        phaseInfo.order = "2X";
+                        phaseInfo.number = 2.5
                     }
                     break;
                 case 'Phase 3':
                     if (firstPhase == 'Phase 2') {
-                        phaseInfo.order = "3A";
-                        phaseInfo.group = "Phase 2/3";
+                        phaseInfo.name = "Phase 2/3";
+                        phaseInfo.number = 3.25;
                     } else {
-                        phaseInfo.order = "3X";
+                        phaseInfo.number = 3.5;
                     }
                     break;
                 case 'Phase 4':
-                    phaseInfo.order = "4X";
+                    phaseInfo.number = 4.5;
                     break;
                 case 'Not Applicable':
-                    phaseInfo.order = "0N";
+                    phaseInfo.number = 0.1;
                     break;
                 default:
                     if (studyType == "Observational") {
-                        phaseInfo.order = "0O";
-                        phaseInfo.group = "Observational";
+                        phaseInfo.name = "Observational";
+                        phaseInfo.number = 0.3;
+
                     } else if (studyType == "Expanded Access") {
-                        phaseInfo.order = "5X";
-                        phaseInfo.group = "Expanded Access";
+                        phaseInfo.name = "Expanded Access";
+                        phaseInfo.number = 4.8;
                     } else {
-                        phaseInfo.order = "0X";
+                        phaseInfo.number = 0.2;
                     }
                     break;
             }
@@ -117,11 +137,39 @@
         return phaseInfo;
     }
 
-    export function getInterventions(trial) {
-        var interventions = trial.InterventionName.filter((intervention, index)=> {
+    export function getInterventions(trial, hidePlacebo) {
+        var interventions = trial.InterventionName;
+        if (hidePlacebo) {
+            interventions = interventions.filter((intervention, index)=> {
             var compare = intervention.toLowerCase();
             return (!compare.includes("placebo") && !compare.startsWith("sham") && !compare.includes("standard care") && !compare.includes("standard of care") && !compare.includes("standard-of-care"));
-        });
+            });
+        }
+
         interventions = interventions.join(', ');
         return interventions;
+    }
+
+    export function cleanSponsor(sponsorName) {
+        sponsorName = sponsorName.toString().replace(", Inc.","").replace(" Ltd","").replace(" Inc.","").replace(", Inc", "").replace(", LLC","").replace(" LLC","");
+        return sponsorName;
+    }
+
+    export function firstFew(conditions, style) {
+        var useThese;
+        if (conditions.length > 3) {
+          useThese = [];
+          useThese.push(conditions[0]);
+          useThese.push(conditions[1]);
+          useThese.push(conditions[2]);
+          useThese.push("...");
+        } else {
+          useThese = conditions;
+        }
+  
+        if (style === ",") {
+          return useThese.join(", ");
+        } else if (style === "*") {
+          return "*" + useThese.join("\n *");
+        }
     }
--- src/Trials.js
+++ src/Trials.js
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { fetchTrialsData, fetchPubMedData, getInterventions, GetData } from "./TrialUtilities.js";
+import { fetchTrialsData, fetchPubMedData, getInterventions, GetData, cleanSponsor, firstFew } from "./TrialUtilities.js";
 
 async function expandUrls (url) {
   var urlToGet = new URL(url, window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + window.location.pathname + "/");
   if (!urlToGet.toString().endsWith(".json")) {
     urlToGet = urlToGet + ".json";
   }
-  var ffbTrials = await GetData(urlToGet);
+
+  var dataAnnotations = await GetData(urlToGet);
   var NCTIds = [];
-  ffbTrials.forEach(trialInfo => {
+  dataAnnotations.data.forEach(trialInfo => {
     var ids = trialInfo.trial.split(',');
     ids.forEach(NCTId => {
+      if (NCTId != "")
       NCTIds.push(NCTId);
     });
   });
+  
   var find = "{" + url + "}";
   var replace = "(" + NCTIds.join(" OR ") + ")";
   return ({
     regEx: new RegExp(find, "ig"),
-    replace: replace
+    replace: replace,
+    dataAnnotations: dataAnnotations
   });
 }
 
@@ -33,20 +37,28 @@ async function expandAndPolishQuery(query) {
       found.push( curMatch[1] );
   }
 
+  var dataAnnotations = null;
   for (var i = 0; i < found.length; i++) {
     var expand = await expandUrls(found[i]);
     query = query.replace(expand.regEx, expand.replace);
+
+    //TODO: this currently only uses one dataAnnotations. More than 1 could be used.
+    dataAnnotations = expand.dataAnnotations;
   }
 
   query = query.replaceAll(" or ", " OR ").replaceAll(" and ", " AND ").replaceAll(" not ", " NOT ");
-  return query;
+  return ({
+    query: query,
+    dataAnnotations: dataAnnotations
+  });
 }
 
 export default function Trials(props) {
     const [lastQuery, setLastQuery] = useState(null);
     const [trials, setTrials] = useState(null);
-    const [showClosed, setShowClosed] = useState(false);
+    const [hideClosed, setHideClosed] = useState(false);
     const [activeGrouping, setActiveGrouping] = useState(0);
+    const [activeView, setActiveView] = useState(0);
     const [trialCount, setTrialCount] = useState(0);
     const [pubmedCount, setPubmedCount] = useState(0);
     const [fetchedTrials, setFetchedTrials] = useState(null);
@@ -55,8 +67,8 @@ export default function Trials(props) {
       fetch(); 
       async function fetch() {
         if (props.query !== lastQuery) {
-          var query = await expandAndPolishQuery(props.query);
-          var fetchedTrials = await fetchTrialsData(query);
+          var queryInfo = await expandAndPolishQuery(props.query);
+          var fetchedTrials = await fetchTrialsData(queryInfo.query, queryInfo.dataAnnotations);
           setFetchedTrials(fetchedTrials);
           setLastQuery(props.query);
           document.title = "'" + props.query + "' Trials";
@@ -76,25 +88,42 @@ export default function Trials(props) {
 
      useEffect(() => {
       if (fetchedTrials !== null) {
-        var trials = fetchedTrials.filter(
-          trial => showClosed ||
+        var filteredTrials = fetchedTrials.filter(
+          trial => !hideClosed ||
           (trial.OverallStatus.toString() != "Completed" &&
             trial.OverallStatus.toString() != "No longer available" && 
             trial.OverallStatus.toString() != "Unknown status" && 
             trial.OverallStatus.toString() != "Withdrawn" && 
-            trial.OverallStatus.toString() != "Terminated"))
-          .sort(groupings[activeGrouping].compare);
+            trial.OverallStatus.toString() != "Terminated"));
+        var trials = groupings[activeGrouping].name !== "None" ? filteredTrials.sort(groupings[activeGrouping].compare) : filteredTrials;
         setTrials(trials);
         setTrialCount(trials.length);
       }
-    }, [fetchedTrials, activeGrouping, showClosed]);
+    }, [fetchedTrials, activeGrouping, hideClosed, activeView]);
+
+    function rankApproach(trial) {
+      var approach = trial.annotations?.approach;
+      switch (approach) {
+        case "Gene Therapies": return 1;
+        case "RNA/Other": return 2;
+        case "Cell-Based Therapies": return 3;
+        case "Small Molecules": return 4;
+        default: 
+          return 5;
+      }
+    }
 
     var groupings = [
       {
+        name: "None",
+        groupBy: null,
+        compare: null
+      },     
+      {
         name: "Phase (desc)",
-        groupBy: "phaseInfo.group".split('.'),
+        groupBy: "phaseInfo.name".split('.'),
         compare: function (trialA, trialB) { 
-          return  trialA.phaseInfo.order > trialB.phaseInfo.order ? -1 : 1
+          return  trialA.phaseInfo.number > trialB.phaseInfo.number ? -1 : 1
           || new Date(trialA.LastUpdatePostDate) - new Date(trialB.LastUpdatePostDate)
         }
       },
@@ -103,74 +132,204 @@ export default function Trials(props) {
         groupBy: "LeadSponsorName".split('.'),
         compare: function (trialA, trialB) { 
           return (trialA.LeadSponsorName < trialB.LeadSponsorName ? -1 : 1)
-          || (trialA.phaseInfo.group > trialB.phaseInfo.group ? -1 : 1)
-          || (new Date(trialA.LastUpdatePostDate) - new Date(trialB.LastUpdatePostDate))
+        }
+      },
+      {
+        name: "Approach",
+        groupBy: "annotations.approach".split('.'),
+        compare: function (trialA, trialB) { 
+          return (rankApproach(trialA) < rankApproach(trialB) ? -1 : 1)
+        }
+      },
+      {
+        name: "Condition",
+        groupBy: "annotations.condition".split('.'),
+        compare: function (trialA, trialB) { 
+          return (trialA.annotations?.condition < trialB.annotations?.condition ? -1 : 1)
         }
       }
     ];
-    var lastGroup = null;
-    
+
+    var views = [
+      {
+        name: 'ClinicalTrials.gov (classic)',
+        method: clinicalTrialsGovStyle,
+      },
+      {
+        name: 'Table (classic)',
+        method: tableStyle1,
+      },
+      {
+        name: 'Table (new)',
+        method: tableStyle2,
+      },
+      {
+        name: 'Default',
+        method: defaultStyle,
+      },
+
+      {
+        name: 'Arrow',
+        method: arrowStyle,
+      },
+    ];
+
     function chooseGrouping(e) {
         setActiveGrouping(Number(e.target.selectedIndex));
     }
-
-    function showClosedChanged(e) {
-        setShowClosed(e.target.checked);
+    
+    function chooseView(e) {
+      setActiveView(Number(e.target.selectedIndex));
     }
 
-    function firstFew(conditions) {
-      if (conditions.length > 3) {
-        return conditions[0] + ", " + conditions[1] + ", " + conditions[2] + ", ...";
-      } else {
-        return conditions.join(", ");
-      }
+    function hideClosedChanged(e) {
+        setHideClosed(e.target.checked);
     }
 
+    function defaultStyle(trial, groupingHeader) {
+      var status = trial.OverallStatus;
+      if (status == "Completed" || status == "Terminated") status = status + " " + new Date(trial.endDate).getFullYear();
+      if (status == "Unknown status") status = "Unknown " + new Date(trial.LastUpdatePostDate).getFullYear();
+      return <>
+              {groupingHeader}
+              <div key={trial.NCTId[0]} className="trial">
+                  <div className={'oldstatus '+trial.OverallStatusStyle}>{activeGrouping == 1 ? trial.phaseInfo.name+"-":false}{status}</div>
+                  <div className='interventionDiv intervention'><span>{getInterventions(trial, true)}</span></div>
+                  { activeGrouping == 1 ? <div className='interventionDiv oldsponsor'><span> ({trial.LeadSponsorName})</span></div> : false }
+                  <div className='title'><a href={'https://beta.clinicaltrials.gov/study/'+trial.NCTId[0]}>{trial.NCTId[0]}</a> : <span>{trial.BriefTitle}</span></div>
+                  <div className='title'>Conditions: {firstFew(trial.Condition)}</div>
+              </div></>
+    }
+
+    function tableStyle1(trial, groupingHeader) {
+      return tableStyle(trial, groupingHeader, false);
+    }
+    function tableStyle2(trial, groupingHeader) {
+      return tableStyle(trial, groupingHeader, true);
+    }
+    function tableStyle(trial, groupingHeader, modern) {
+      var status = trial.OverallStatus;
+      if (status == "Completed" || status == "Terminated") status = status + " " + new Date(trial.endDate).getFullYear();
+      if (status == "Unknown status") status = "Unknown " + new Date(trial.LastUpdatePostDate).getFullYear();
+      var trialStyle =  trial.closed ? "trial tal closedRow" : "trial tal";
+      return <>
+              {groupingHeader}
+              <div key={trial.NCTId[0]} className={trialStyle}>
+                  <span className='tal w200'>
+
+                  {trial.annotations != null ? trial.annotations.condition : firstFew(trial.Condition)}
+                  {trial.annotations != null && trial.annotations.approachDetail != null ? " (" + trial.annotations.approachDetail +") - " : " - "}
+                  <a href={trial.annotations?.sponsorLink}>{ true ? (trial.annotations?.sponsor != null ? trial.annotations.sponsor : cleanSponsor(trial.LeadSponsorName)) : cleanSponsor(trial.LeadSponsorName)}</a>
+                  </span>
+                  <span className='tal w50'>
+                    {trial.phaseInfo.name} 
+                  </span>
+                  { modern ? <span className='tal w50'>
+                    <a href={'https://beta.clinicaltrials.gov/study/'+trial.NCTId[0]}>{trial.NCTId[0]}</a>
+                  </span> : false }
+                  
+                  
+              </div></>
+    }
+
+    function clinicalTrialsGovStyle(trial, groupingHeader) {
+      var status = trial.OverallStatus;
+      if (status == "Completed" || status == "Terminated") status = status + " " + new Date(trial.endDate).getFullYear();
+      if (status == "Unknown status") status = "Unknown " + new Date(trial.LastUpdatePostDate).getFullYear();
+      var trialStyle =  trial.closed ? "trial tal closedRow" : "trial tal";
+      return <>
+              {groupingHeader}
+              <div key={trial.NCTId[0]} className={trialStyle}>
+                  <span className='tal w50'>
+                    {trial.OverallStatus} 
+                  </span>
+                  <span className='tal w200'>
+                    <a href={'https://clinicaltrials.gov/ct2/show/'+trial.NCTId[0]}>{trial.BriefTitle}</a>
+                  </span>
+                  <span className='tal w50'>
+                    {firstFew(trial.Condition, "*")} 
+                  </span>
+                  <span className='tal w50'>
+                    {getInterventions(trial, false)} 
+                  </span>
+                  <span className='tal w50'>
+                    {trial.phaseInfo.name} 
+                  </span>
+                  <span className='tal w100'>
+                    {firstFew(trial.LocationFacility,",")}
+                  </span>
+              </div></>
+    }
+
+    function arrowStyle(trial, groupingHeader) {
+      var arrowColor = trial.closed ? 'grey' : 'lightgreen';
+      var status = trial.OverallStatus;
+      if (status == "Completed" || status == "Terminated") status = status + " " + new Date(trial.endDate).getFullYear();
+      if (status == "Unknown status") status = "Unknown " + new Date(trial.LastUpdatePostDate).getFullYear();
+      return <>
+              {groupingHeader}
+              <div key={trial.NCTId[0]} className="trial container">
+                  <svg xmlns="http://www.w3.org/2000/svg" className='arrow'>
+                    <defs>
+                      <marker id="arrowhead" markerWidth="1" markerHeight="1" 
+                      refX="0" refY=".5" orient="auto">
+                        <polygon points="0 0, 1 .5, 0 1" fill={arrowColor} />
+                      </marker>
+                    </defs>
+                    <line x1="0%" y1="15%" x2={90 * trial.phaseInfo.number / 5.0 + '%'} y2="15%" stroke={arrowColor} 
+                    strokeWidth="20" markerEnd={ trial.closed ? "" : "url(#arrowhead)"}/>
+                  </svg>
+                  <div className='info nct'><a href={'https://beta.clinicaltrials.gov/study/'+trial.NCTId[0]}>{trial.NCTId[0]}</a></div>
+                  <div className='info interventions'><span>{getInterventions(trial, true)}</span></div>
+                  { false ? <span className='info sponsor'>{trial.LeadSponsorName}</span> : false }
+                  <div className={'info status '}>{status}</div>
+                  <div className='info approach'>{trial.annotations?.condition != null ? trial.annotations.condition : firstFew(trial.Condition)} {trial.annotations?.geneTarget != null ? "(" + trial.annotations?.geneTarget + ")" : null}
+                    {' - '}<a href={trial.annotations?.sponsorLink}>{trial.annotations?.sponsor != null ? trial.annotations.sponsor : trial.LeadSponsorName}</a></div>
+              </div></>
+    }
+
+    var lastGroup = null;
     var tooManyWarning = trialCount > 6000 ? " [revise terms, only 6000 shown]" : "";
     return <>
         <div className='tm10'>&nbsp;
           { trials !== null ? <>
           <span className='hitCounts'>{"ClinicalTrials.gov (" + trialCount + tooManyWarning + ") |"}</span>{' '}
-          <a className='hitCounts' href={'https://pubmed.ncbi.nlm.nih.gov/?term='+props.query}>PubMed.gov{" (" + pubmedCount + ") | "}</a>{' '}
-          <a href='https://buymeacoffee.com/rrelyea'>Sponsor this site</a>{" | "}<a href='mailto:rob@relyeas.net'>Feature request</a>
+          <a className='hitCounts' href={'https://pubmed.ncbi.nlm.nih.gov/?term='+props.query}>PubMed.gov{" (" + pubmedCount + ")"}</a>          
           </> : false }
         </div>
         <div className='status'></div>
 
-        <div>
-          <label><input type='checkbox' defaultValue={showClosed} onChange={(e) => showClosedChanged(e)} /><span id='showClosedLabel'>Show Closed {!showClosed && fetchedTrials !== null ? "(" + (fetchedTrials.length - trialCount) + ")": false}</span></label>
-          <label className='lm10'>Group by:</label>{' '}
+        <div className='tbm10'>
+          <label><input type='checkbox' defaultValue={hideClosed} onChange={(e) => hideClosedChanged(e)} /><span id='showClosedLabel'>Hide Closed</span></label>
+          <label className='lm10'>Sort by:</label>{' '}
           <select onChange={(e) => chooseGrouping(e)}>
           {groupings.map((grouping)=> <option>{grouping.name}</option>)}
+          </select>
+          <label className='lm10'>View:</label>{' '}
+          <select onChange={(e) => chooseView(e)}>
+          {views.map((view)=> <option>{view.name}</option>)}
           </select>
         </div>  
         {trials !== null ? Object.entries(trials).map(([k,trial], lastPhase) =>
           {
             var groupingHeader = null;
-            var groupingValue = trial;
-            for (var i = 0; i < groupings[activeGrouping].groupBy.length; i++) {
-              groupingValue = groupingValue[groupings[activeGrouping].groupBy[i]];
-            }
-            
-            groupingValue = groupingValue.toString();
-            if (groupingValue != lastGroup) {
-              groupingHeader = <h2 className='phase'>{groupingValue}</h2>;
-            } 
+            if (groupings[activeGrouping].name !== "None") {
+              var groupingValue = trial;
+              for (var i = 0; i < groupings[activeGrouping].groupBy.length; i++) {
+                groupingValue = groupingValue[groupings[activeGrouping].groupBy[i]];
+                if (groupingValue == null) { break; }
+              }
 
-            lastGroup = groupingValue;
-            var status = trial.OverallStatus;
-            if (status == "Completed" || status == "Terminated") status = status + " " + new Date(trial.endDate).getFullYear();
-            if (status == "Unknown status") status = "Unknown " + new Date(trial.LastUpdatePostDate).getFullYear();
+              groupingValue = groupingValue != null ? groupingValue.toString() : groupings[activeGrouping].name + " Type Missing";
+              if (groupingValue != lastGroup) {
+                groupingHeader = <div className='group'>{groupingValue}</div>;
+              } 
+
+              lastGroup = groupingValue;
+            }
+
+            return views[activeView].method(trial, groupingHeader);
             
-            return <>
-              {groupingHeader}
-              <div key={trial.NCTId[0]} className="trial">
-                <div className={'status '+trial.OverallStatusStyle}>{activeGrouping == 1 ? trial.phaseInfo.group+"-":false}{status}</div>
-                <div className='interventionDiv intervention'><span>{getInterventions(trial)}</span></div>
-                { activeGrouping == 0 ? <div className='interventionDiv sponsor'><span> ({trial.LeadSponsorName})</span></div> : false }
-                <div className='title'><a href={'https://beta.clinicaltrials.gov/study/'+trial.NCTId[0]}>{trial.NCTId[0]}</a> : <span>{trial.BriefTitle}</span></div>
-                <div className='title'>Conditions: {firstFew(trial.Condition)}</div>
-              </div></>
           })
         : <h3>searching for trials...</h3>}  
       </>;
